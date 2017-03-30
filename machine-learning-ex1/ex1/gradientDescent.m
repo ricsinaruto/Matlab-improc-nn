38d7fc9ba864349f98304cac832aaddccdc4e7b3
Csáky Richárd Krisztián <ricsinaruto@hotmail.com>
2017-03-30 11:20:11 +0200

    linear regression exercise

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,7 +16,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
+    cost=0;
+    for i=1:m
+        h0=theta'*X(i,:)';
+        cost=cost+(h0-y(i))*X(i,:)';
+    end
+    
+    theta=theta-alpha/m*cost;
 
 
 
