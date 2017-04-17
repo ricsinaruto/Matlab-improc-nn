67d1ed2248a73ee6f11e75bd7d6b405a4e54bd4f
Csáky Richárd Krisztián <ricsinaruto@hotmail.com>
2017-04-17 19:40:52 +0200

    deleted:    meres/1.12 t=0.0032/sv_1_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/1.12 t=0.0032/sv_2_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/1.12 t=0.0032/sv_3_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/1.12 t=0.0032/sv_4_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.12/0.0032 tc/desktop.ini 	new file:   meres/1.12/0.0032 tc/open_poz.jpg 	new file:   meres/1.12/0.0032 tc/open_torque.jpg 	new file:   meres/1.12/0.0032 tc/open_vel.jpg 	new file:   meres/1.12/0.0032 tc/plotting.m 	new file:   meres/1.12/0.0032 tc/sv_1_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.12/0.0032 tc/sv_2_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.12/0.0032 tc/sv_3_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.12/0.0032 tc/sv_4_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.12/0.032/desktop.ini 	new file:   meres/1.12/0.032/open_poz.jpg 	new file:   meres/1.12/0.032/open_torque.jpg 	new file:   meres/1.12/0.032/open_vel.jpg 	new file:   meres/1.12/0.032/plotting.m 	new file:   meres/1.12/0.032/sv_1_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.12/0.032/sv_2_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.12/0.032/sv_3_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.12/0.032/sv_4_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.13/0.1/open_poz.jpg 	new file:   meres/1.13/0.1/open_torque.jpg 	new file:   meres/1.13/0.1/open_vel.jpg 	new file:   meres/1.13/0.1/plotting.m 	new file:   meres/1.13/0.1/sv_1_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.13/0.1/sv_2_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.13/0.1/sv_3_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.13/0.1/sv_4_jmkpqwe20iinyo45dirb1w3p.m 	new file:   meres/1.13/0.1/sv_5_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/1.625696969/all_jmkpqwe20iinyo45dirb1w3p (1).zip 	deleted:    meres/1.625696969/controller_jmkpqwe20iinyo45dirb1w3p.h 	deleted:    meres/1.625696969/graph_StateVariable_5_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/1.625696969/graph_StateVariable_6_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/1.625696969/graph_StateVariable_7_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/1.625696969/graph_position_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/1.625696969/graph_time_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/1.625696969/graph_torque_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/1.625696969/graph_velocity_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/1.625696969/sv_1_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/1.625696969/sv_2_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/1.625696969/sv_3_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/1.625696969/sv_4_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/1.625696969/sv_5_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/1.625696969/sv_6_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/1.625696969/sv_7_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/P 2.85/all_jmkpqwe20iinyo45dirb1w3p.zip 	deleted:    meres/P 2.85/controller_jmkpqwe20iinyo45dirb1w3p.h 	deleted:    meres/P 2.85/graph_StateVariable_5_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/P 2.85/graph_StateVariable_6_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/P 2.85/graph_position_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/P 2.85/graph_time_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/P 2.85/graph_torque_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/P 2.85/graph_velocity_jmkpqwe20iinyo45dirb1w3p.jpg 	deleted:    meres/P 2.85/sv_1_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/P 2.85/sv_2_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/P 2.85/sv_3_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/P 2.85/sv_4_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/P 2.85/sv_5_jmkpqwe20iinyo45dirb1w3p.m 	deleted:    meres/P 2.85/veloc.m 	modified:   meres/convert.m 	modified:   meres/convert_bessel.m 	deleted:    meres/position.m 	deleted:    meres/time.m 	deleted:    meres/torque.m 	deleted:    meres/velocity.m

---
+++ meres/1.12/0.032/plotting.m
@@ -0,0 +1,37 @@
+sv_1_jmkpqwe20iinyo45dirb1w3p
+sv_2_jmkpqwe20iinyo45dirb1w3p
+sv_3_jmkpqwe20iinyo45dirb1w3p
+sv_4_jmkpqwe20iinyo45dirb1w3p
+ 
+
+titlu='Fordulatszám szurés';
+time=time/1000;
+plot(time,position);
+set(gca, 'fontsize', [12]);
+xlabel('Time [sec]');
+ylabel('Position [rad]');
+title(titlu);
+% you can adjust your axis
+grid
+print -djpeg open_poz
+ 
+ 
+plot(time,velocity);
+set(gca, 'fontsize', [12]);
+xlabel('Time [sec]');
+ylabel('Velocity [rad/s]');
+title(titlu);
+% you can adjust your axis
+grid
+print -djpeg open_vel
+ 
+plot(time,torque)
+set(gca, 'fontsize', [12]);
+xlabel('Time [sec]');
+ylabel('Torque [Nm]');
+title(titlu);
+% you can adjust your axis
+grid
+print -djpeg open_torque
+ 
+
